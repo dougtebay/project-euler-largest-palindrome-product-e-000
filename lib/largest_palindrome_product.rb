99ea24ce63394d012ea26fe7902f1803b7fdc557
dougtebay <dougtebay@gmail.com>
2015-12-16 09:38:19 -0500

    All tests passing

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+  largest = 0
+  900.upto(999).each do |x|
+    900.upto(999).each do |y|
+      largest = x * y if x * y == (x * y).to_s.reverse.to_i && x * y > largest
+    end
+  end
+  largest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,16 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize
+    @answer = 0
+    900.upto(999).each do |x|
+      900.upto(999).each do |y|
+        @answer = x * y if x * y == (x * y).to_s.reverse.to_i && x * y > @answer
+      end
+    end
+    @answer
+  end
+
+  def answer
+    @answer
+  end
+end
